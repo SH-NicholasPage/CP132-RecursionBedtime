5314c0d106531ea3e0639226a215abcb4c820dc3
SH-NicholasPage <npage@southhills.edu>
2022-11-06 22:14:11 -0500

    Initial commit

---
+++ RecursiveBedtime/RecursiveBedtime/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+
+namespace RecursiveBedtime
+{
+    class Program
+    {
+        static void Main()
+        {
+            //TODO: Your code here
+
+            Console.WriteLine("The story has concluded. Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
